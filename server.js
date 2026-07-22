cf84f20b36f1fc5a4c68ca7f598d956ecd39582e
githubscientist <sathish.datascientist@gmail.com>
2026-07-22 19:09:07 +0530

    express server setup --done

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // setup mongodb database connection
 // import mongoose
 const mongoose = require('mongoose');
-const { MONGODB_URI } = require('./utils/config');
+const { MONGODB_URI, PORT, HOST } = require('./utils/config');
+const app = require('./app');
 
 // connect to mongodb database
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
+
+        // start the server after successful database connection
+        app
+            .listen(PORT, HOST, () => {
+                console.log(`Server running on http://${HOST}:${PORT}`);
+            })
+            .on('error', (error) => {
+                console.error('Error starting the server:', error.message);
+            })
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error.message);
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const ENV = process.env.ENV;
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
 
 module.exports = {
     MONGODB_URI,
+    ENV,
+    HOST,
+    PORT,
 }
